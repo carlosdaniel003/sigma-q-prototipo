23854e3633f64e3ae229901170d1236a7e048339
Carlos Daniel Simoes dos Santos <cdaniel@MKMONDIAL.local>
2025-11-06 09:24:32 -0400

    Limpeza de arquivos antigos e atualização da estrutura com app/main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from utils import carregar_dados
-
-print("🚀 Iniciando leitura da base...")
-df = carregar_dados()  # ele vai procurar automaticamente o base_de_dados.xlsx
-print("✅ Dados lidos com sucesso!")
-print(df.head())
--- ver_colunas.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-arquivo = r"C:\Users\cdaniel\Documents\SIGMA-Q PROTÓTIPO\data\base_de_dados.xlsx"
-
-df = pd.read_excel(arquivo)
-print("🔍 Colunas encontradas no arquivo:")
-for c in df.columns:
-    print(f"- '{c}'")
